fe2e5e36b745f8e39627b2dc3d785f1838bd9e58
TSchultz55 <TSchultz55@636f4772-cc2d-0410-aea8-55be4c6ad4b4>
2008-01-31 00:01:36 +0000

    - Added functionality to deserialize directly from a given URL - Added functionality to deserialzie directly from a File object - Added package for benchmarking tests for performance measuring. Cheers!

---
+++ trunk/Java/APMLStream/APMLStream-0.1/src/org/apml/base/exceptions/InvalidAPMLFormatException.java
@@ -0,0 +1,36 @@
+/*	
+ *  APMLStream: Java library for APML serialization/deserialization
+ *	By:  Tim Schultz
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License"); 
+ *	you may not use this file except in compliance with the License. 
+ *	You may obtain a copy of the License at 
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ *	Unless required by applicable law or agreed to in writing, software 
+ *	distributed under the License is distributed on an "AS IS" BASIS, 
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ *	See the License for the specific language governing permissions and 
+ *	limitations under the License.
+ *
+ */
+
+package org.apml.base.exceptions;
+
+/**
+ * <p>Thrown when a specified APML file is not valid APML</p> 
+ * @author Tim Schultz
+ */
+public class InvalidAPMLFormatException extends Exception
+{
+	public InvalidAPMLFormatException()
+	{
+		super();
+	}
+	
+	public InvalidAPMLFormatException(String msg)
+	{
+		super(msg);
+	}
+}
